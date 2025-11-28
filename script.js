3d7cf4984caf8bfce543efca80b4507fbf34d571
brenno-fullstack <brenno.codes@gmail.com>
2025-11-27 23:10:57 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function enviarWhats(envet) {
+    event.preventDefault()
+
+    const nome = document.getElementById('nome').value;
+    const mensagem = document.getElementById('mensagem').value;
+    const telefone = '5511979569481'
+    
+    const texto = `Olá! Me chamo ${nome}, ${mensagem} `
+    const msgFormatada = encodeURIComponent(texto)
+    
+    const url = `https://wa.me/${telefone}?text=${msgFormatada}`
+
+    console.log (url)
+    
+    window.open(url, '_blank')
+
+}
